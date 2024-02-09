5870be6aa896417a97167932353131e6312c4864
Anilykh <156141337+Anilykh@users.noreply.github.com>
2024-02-09 15:16:38 +0100

    Add files via upload

---
+++ Test.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+
+public class ConverterTests
+{
+    private ConverterTests converter;
+
+    public void Setup()
+    {
+        converter = new ConverterTests();
+    }
+
+    public void InchesToMm_Conversion()
+    {
+        double result = converter.converter(1, "-mm");
+        Assert.AreEqual(25., result);
+    }
+
+    public void InchesToCm_Conversion()
+    {
+        double result = converter.converter(1, "-cm");
+        Assert.AreEqual(2.54, result);
+    }
+
+    public void InchesToM_Conversion()
+    {
+        double result = converter.converter(1, "-m");
+        Assert.AreEqual(0.0254, result);
+    }
+}
